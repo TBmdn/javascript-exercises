f01748d144afe9c1b055fa958f1e25546b881ba1
TBmdn <tboumeddine@gmail.com>
2024-07-26 09:05:10 +0100

    Solving 02 exercise
    it's a function that repeats a string in a given number times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    let repetedString = string;
+    if(num == 0){
+        return '';
+    }
+    else if (num < 0) {
+        return 'ERROR';
+    }
+    else{
+        for(let i = 1; i < num; i++){
+            repetedString = repetedString.concat(string);
+        }
+        return repetedString;
+    }
 };
 
 // Do not edit below this line
