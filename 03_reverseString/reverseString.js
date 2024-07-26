368cbf5cbf54713ab5a3b248a5f11029ebcf6fde
TBmdn <tboumeddine@gmail.com>
2024-07-26 20:26:47 +0100

    Solve the third exercise
    coding a function that reverse a string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,20 @@
-const reverseString = function() {
-
+const reverseString = function(enteredString) {
+    /* the string which will reverse the string on it */
+    let reString = '';
+    if(enteredString.length == 0){
+        return reString;
+    }
+    else{
+        for(let i = (enteredString.length - 1); i >= 0; i--){
+            if(reString == ''){
+                reString = enteredString[i];
+            }
+            else{
+                reString += enteredString[i];
+            }
+        }
+        return reString;
+    }
 };
 
 // Do not edit below this line
