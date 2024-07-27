6253a29c1a669c5fe3f88a7e92fe4fd4b96945f1
TBmdn <tboumeddine@gmail.com>
2024-07-27 10:20:40 +0100

    Solve the fourth exercise
    code a function to removes elements from an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...wantToDlt) {
+    for (const i in wantToDlt) {
+        // counter for looping throug the array with while loop
+        let j = 0;
+        while (j != array.length) {
+            if (array[j] === wantToDlt[i]) {
+                array.splice(j,1);
+            }
+            else {
+                j++;
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
