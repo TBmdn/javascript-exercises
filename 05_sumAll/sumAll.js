ca302bc0d782a40a5a837667f7b2a665b5d3383f
TBmdn <tboumeddine@gmail.com>
2024-07-27 17:36:44 +0100

    Solve the fifth exercise
    code a function to calculate the sum between and inclucde two positive integers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(num1,num2) {
+    let sum;
+    // to return ERROR in case of not integer or negetive values is entered 
+    if( !(Number.isInteger(num1)) || !(Number.isInteger(num2)) || num1 < 0 || num2 < 0) {
+        return 'ERROR'
+    }
+    else {
+        const firstOperend = Math.min(num1,num2);
+        sum = firstOperend;
+        let i = 1;
+        while (firstOperend + i <= Math.max(num1,num2)) {
+            sum += firstOperend + i;
+            i++;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
