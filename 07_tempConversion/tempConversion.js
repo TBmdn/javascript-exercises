e5529f323a858be2736dc1511c975a0de79b321b
TBmdn <tboumeddine@gmail.com>
2024-07-28 05:55:16 +0100

    Solve the seventh exercise
    code two functions to convert from celsius degree to fahrenheit and vice versa

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempDegree) {
+  return Math.round((tempDegree-32) * 5/9 * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempDegree) {
+  return Math.round(((tempDegree * 9/5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
